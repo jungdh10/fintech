c491a10fcd6eef93f6cce7e094c7399bb5da32e2
jungdh10 <dhh0089@gmail.com>
2020-10-22 13:59:18 +0900

    naver -> weather api 변경

--- request.js
+++ request.js
@@ -1,4 +1,7 @@
 const request = require('request');
-request('http://www.naver.com', function (error, response, body) {
-  console.log('body:', body); // Print the HTML for the Google homepage.
-});
+request(
+  "https://www.weather.go.kr/weather/forecast/mid-term-rss3.jsp?stnld=109",
+  function (error, response, body) {
+    console.log("body:", body);
+  }
+);
