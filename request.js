3c94666ca99aecdd877b0330d0d4962ee29f85f6
jungdh10 <dhh0089@gmail.com>
2020-10-24 12:40:01 +0900

    날씨 api 데이터 출력

--- request.js
+++ request.js
@@ -1,7 +1,14 @@
 const request = require('request');
+var parseString = require('xml2js').parseString;
+
 request(
   "https://www.weather.go.kr/weather/forecast/mid-term-rss3.jsp?stnld=109",
   function (error, response, body) {
-    console.log("body:", body);
+    var xml = body;
+    parseString(xml, function (err, result) {
+      console.dir(result.rss.channel);
+      ////work5 기상 예보 내역을 출력하기 [wf] 안에 있는 내용 출력
+      
+    });
   }
 );
