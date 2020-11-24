0c23995abd9916815d553dfe95a82c153b7e7f9a
jungdh10 <dhh0089@gmail.com>
2020-11-24 23:28:19 +0900

    사용자 데이터를 받기위한 EXPRESS 옵션 설정

--- express_server.js
+++ express_server.js
@@ -1,20 +1,24 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
 app.set("views", __dirname + "/views"); //ejs 를 사용하기위한 디렉토리 설정
 app.set("view engine", "ejs"); //ejs 를 사용하기위한 뷰 엔진 설정
+
+app.use(express.json()); //JSON 타입의 데이터를 받기위한 설정
+app.use(express.urlencoded({extended: false})); //urlencoded 타입의 데이터를 받기위한 설정
  
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
 
 app.get("/ejsTest", function (req, res) {
-    res.render("test");
+  res.render("test");
 });
 
-app.get("/getDataTest", function (req, res) {
-  var userText = req.body.userTest;
-  consol.log(userText);
+app.post("/getDataTest", function (req, res) {
+  var userText = req.body.userText;
+  console.log(userText);
+  res.json("입력값은 : " + userText);
 });
 
 
