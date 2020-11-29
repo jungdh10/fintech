23578e0cf48b9ffd6bb91f838d70fd58917c065f
jungdh10 <dhh0089@gmail.com>
2020-11-29 22:12:32 +0900

    디자인 파일 적용

--- express_server.js
+++ express_server.js
@@ -6,6 +6,9 @@ app.set("view engine", "ejs"); //ejs 를 사용하기위한 뷰 엔진 설정
 
 app.use(express.json()); //JSON 타입의 데이터를 받기위한 설정
 app.use(express.urlencoded({extended: false})); //urlencoded 타입의 데이터를 받기위한 설정
+
+app.use(express.static(path.join(__dirname, "public")));
+//to use static asset 디자인 파일 위치를 정의
  
 app.get("/", function (req, res) {
   res.send("Hello World");
@@ -18,7 +21,7 @@ app.get("/ejsTest", function (req, res) {
 app.post("/getDataTest", function (req, res) {
   var userText = req.body.userText;
   console.log(req.body);
-  console.log(userText);
+  console.log(userText, req.body.sendTime);
   res.json("입력값은 : " + userText);
 });
 
