e8cec24435f91d0e9ac42a98b63866179f871082
jungdh10 <dhh0089@gmail.com>
2021-01-07 13:48:48 +0900

    계좌 등록 내역 확인 코드 추가

--- express_server.js
+++ express_server.js
@@ -142,7 +142,26 @@ app.post("/login", function (req, res) {
 
 app.post("/list", auth, function (req, res) {
   //https://testapi.openbanking.or.kr/v2.0/user/me url 에 Request 요청하기
-  request(option, function (err, response, body) {});
+  var option = {
+    method: "GET",
+    url: "https://testapi.openbanking.or.kr/v2.0/user/me",
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiIxMTAwNzYzNDY4Iiwic2NvcGUiOlsiaW5xdWlyeSIsImxvZ2luIiwidHJhbnNmZXIiXSwiaXNzIjoiaHR0cHM6Ly93d3cub3BlbmJhbmtpbmcub3Iua3IiLCJleHAiOjE2MTYzMzg4MzQsImp0aSI6IjVkMzEwYWYyLTk0N2ItNGQyYS1iY2VlLTE0OTI3MDRjY2E1ZiJ9.XctYXoQnRYCIIoZ8UVtywLqUOqySYOch8g8c_Heqa2Q",
+    },
+    //accesstoken 입력
+    //form 형태는 form / 쿼리스트링 형태는 qs / json 형태는 json ***
+    qs: {
+      user_seq_no: "1100763468",
+      //#자기 키로 시크릿 변경
+    },
+  };
+
+  request(option, function (err, response, body) {
+    var resResult = JSON.parse(body);
+    //json 문서를 파싱하여 javascript 오브젝트로 변환
+    res.json(resResult);
+  });
 });
 
 app.listen(3000);
