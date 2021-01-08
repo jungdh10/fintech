cf4cdc3ea68d0cc79683ab3f7a54cf171a15c04e
jungdh10 <dhh0089@gmail.com>
2021-01-08 14:13:45 +0900

    멀티 유저 대응 완료

--- express_server.js
+++ express_server.js
@@ -142,25 +142,42 @@ app.post("/login", function (req, res) {
 
 app.post("/list", auth, function (req, res) {
   //https://testapi.openbanking.or.kr/v2.0/user/me url 에 Request 요청하기
-  var option = {
-    method: "GET",
-    url: "https://testapi.openbanking.or.kr/v2.0/user/me",
-    headers: {
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiIxMTAwNzYzNDY4Iiwic2NvcGUiOlsiaW5xdWlyeSIsImxvZ2luIiwidHJhbnNmZXIiXSwiaXNzIjoiaHR0cHM6Ly93d3cub3BlbmJhbmtpbmcub3Iua3IiLCJleHAiOjE2MTYzMzg4MzQsImp0aSI6IjVkMzEwYWYyLTk0N2ItNGQyYS1iY2VlLTE0OTI3MDRjY2E1ZiJ9.XctYXoQnRYCIIoZ8UVtywLqUOqySYOch8g8c_Heqa2Q",
-    },
-    //accesstoken 입력
-    //form 형태는 form / 쿼리스트링 형태는 qs / json 형태는 json ***
-    qs: {
-      user_seq_no: "1100763468",
-      //#자기 키로 시크릿 변경
-    },
-  };
+  var userId = req.decoded.userId;
+  //{ 토큰에 담겨있는 사용자 정보
+  //"userId": 8,
+  //"userEmail": "dhh0089@gmail.com",
+  //"iat": 1610077748,
+  //"exp": 1610509748,
+  //"iss": "fintech.admin",
+  //"sub": "user.login.info"
+  //}
 
-  request(option, function (err, response, body) {
-    var resResult = JSON.parse(body);
-    //json 문서를 파싱하여 javascript 오브젝트로 변환
-    res.json(resResult);
+  connection.query("SELECT * FROM user WHERE id = ?", [userId], function (
+    error,
+    results
+  ) {
+    if (error) throw error;
+    else {
+      var option = {
+        method: "GET",
+        url: "https://testapi.openbanking.or.kr/v2.0/user/me",
+        headers: {
+          Authorization: "Bearer " + results[0].accesstoken,
+        },
+        //accesstoken 입력
+        //form 형태는 form / 쿼리스트링 형태는 qs / json 형태는 json ***
+        qs: {
+          user_seq_no: results[0].userseqno,
+          //#자기 키로 시크릿 변경
+        },
+      };
+      request(option, function (err, response, body) {
+        var resResult = JSON.parse(body);
+        console.log(resResult);
+        //json 문서를 파싱하여 javascript 오브젝트로 변환
+        res.json(resResult);
+      });
+    }
   });
 });
 
